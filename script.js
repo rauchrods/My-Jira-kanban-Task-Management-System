5a2ae1c42ec20e859da8779769fff47862e7702d
rauch <rauchrodrigues@gmail.com>
2023-04-14 00:37:13 +0530

    2nd draft

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let addtaskbtn = document.querySelector(".add-task-btn");
 let modaltask = document.querySelector(".modal");
 let form = document.querySelector("#add-task-form");
 let taskmodalclose = document.querySelector(".close");
+
+
+
+
 addtaskbtn.addEventListener("click", () => {
 
     modaltask.style.display = "flex";
@@ -67,20 +71,21 @@ function addTask(task, priority, date, status) {
 
 
     setPriorityColor();
+    dragactions();
 }
 
 
 function setPriorityColor() {
-   
-    document.querySelectorAll(".task-list").forEach((list)=>{
-        list.querySelectorAll(".priority").forEach((priorityele)=>{
+
+    document.querySelectorAll(".task-list").forEach((list) => {
+        list.querySelectorAll(".priority").forEach((priorityele) => {
             if (priorityele.innerHTML == "high") {
-                priorityele.style.backgroundColor ="#e3242b";
+                priorityele.style.backgroundColor = "#e3242b";
             }
             else if (priorityele.innerHTML == "medium") {
                 priorityele.style.backgroundColor = "orange";
             }
-            else if (priorityele.innerHTML == "low"){
+            else if (priorityele.innerHTML == "low") {
                 priorityele.style.backgroundColor = "#5dbb63";
             }
         })
@@ -88,3 +93,38 @@ function setPriorityColor() {
 
 }
 
+
+function dragactions() {
+    let taskcards = document.querySelectorAll(".task-card");
+
+    taskcards.forEach((taskcard) => {
+        taskcard.draggable = true;
+    })
+
+    taskcards.forEach((taskcard) => {
+        taskcard.addEventListener("dragstart", () => {
+           taskcard.classList.add("dragging");
+        });
+    });
+    
+    taskcards.forEach((taskcard) => {
+        taskcard.addEventListener("dragend", () => {
+           taskcard.classList.remove("dragging");
+        });
+    });
+}
+
+
+
+
+let draggablecontainers = document.querySelectorAll(".task-list");
+
+draggablecontainers.forEach((draggablecontainer) => {
+    draggablecontainer.addEventListener("dragover", (e) => {
+       e.preventDefault();
+       let draggable = document.querySelector(".dragging");
+         console.log(draggable);
+     
+       draggablecontainer.appendChild(draggable);
+    });
+});
